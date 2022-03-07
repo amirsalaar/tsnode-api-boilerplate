55de8b83f1ba310c924fa04cde75cb7e7587602d
Amirsalar Heydari <amirsalaar@gmail.com>
2022-03-07 10:00:30 -0800

    Remove body parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import compression from "compression"; // compresses requests
-import bodyParser from "body-parser";
 import lusca from "lusca";
 import path from "path";
 import passport from "passport";
@@ -17,8 +16,8 @@ const app = express();
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(mongoStoreSession);
 // app.use(passport.initialize());
 // app.use(passport.session());
